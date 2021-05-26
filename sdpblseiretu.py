ebe692ed35207668544e6c62a2b02278bd11c7cd
Keisuke Magara <61485115+Keisuke-Magara@users.noreply.github.com>
2021-05-26 11:53:40 +0900

    Add files via upload

---
+++ sdpblseiretu.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+import codecs
+f=codecs.open('ticket.info','r','utf-8')
+g=codecs.open('ticket.txt','w','utf-8')
+line=f.readline()
+print('商品名','　　　　　　価格','　残り枚数')
+g.writelines(['商品名','		 価格','	残り枚数','\n'])
+while line:
+	x=line.split()
+	y=7-len(x[4])
+	print(x[4],'　'*y,x[5],'円','残り',x[1],'枚')
+	g.writelines([x[4],'　'*y,x[5],'円','残り',x[1],'枚','\n'])
+	line=f.readline()
+	if line=='\n':
+		break
+line=f.readline()
+print(line)
+g.write(line)
+f.close
+g.close
